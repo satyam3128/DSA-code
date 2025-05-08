8e8dc528b69bc4602b7e4f93cb8f9721b811233b
SATYAM SUMAN <143528170+satyam3128@users.noreply.github.com>
2025-05-08 16:17:45 +0000

    odd even checking number

---
+++ 1_BASIC_program/5_even_odd_number.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int n ;
+    cout<<"Enter the Number:";
+    cin>>n;
+    if(n%2 == 0){
+        cout<<n<<" is a Even Number";
+    }
+    else{
+        cout<<n<<" is a Odd Number ";
+    }
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # DSA-code 
 Here i am practicing the code of Dsa That i lernt from my course.
 
+<!-- Vector  -->
+we can say vector is the replacement of Array 
+
+problem with array, it having the fixed size , 
+but it is fast 

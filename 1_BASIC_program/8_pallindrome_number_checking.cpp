3df9c8f8dee012732c9d4871e30cf050b09c1ede
SATYAM SUMAN <143528170+satyam3128@users.noreply.github.com>
2025-05-09 16:15:05 +0000

    pallindrom number checking commit

---
+++ 1_BASIC_program/8_pallindrome_number_checking.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int pallindrome_number_checking(int num){
+    int orig = num;
+    int rem, ans = 0;
+    while(orig){
+        rem = orig %10;
+        orig = orig /10;
+        ans = ans*10 + rem;
+
+    }
+    if(ans == num){
+        return 1;
+    }
+    return 0;
+}
+int main(){
+    int n;
+    cout<<"Enter the Number:";
+    cin>>n;
+    cout<<"Is "<<n<<" a Pallindrome Number : "<<pallindrome_number_checking(n);
+
+    return 0;
+}

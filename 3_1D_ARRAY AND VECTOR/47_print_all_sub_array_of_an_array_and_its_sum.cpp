0fa5168e25b6ffe5c3e12ab4be13e1d2c8697a8d
Satyam3128 <satyam2005suman@gmail.com>
2025-05-14 22:59:55 +0530

    print all sub array sum in 47

--- 3_1D_ARRAY AND VECTOR/47_print_all_sub_array_of_an_array_and_its_sum.cpp
+++ 3_1D_ARRAY AND VECTOR/47_print_all_sub_array_of_an_array_and_its_sum.cpp
@@ -16,6 +16,23 @@ void print_sub_array(vector<int>v){
         cout<<endl;
     }
 }
+void print_sub_array_sum(vector<int>v){
+    int n = v.size();
+    for(int start = 0;start<n ;start++){
+        for(int end = start ; end<n ;end++){
+            cout<<"{ ";
+            int current_sum = 0;
+            for(int i = start ; i<=end ;i++){
+                current_sum +=v[i];
+            }
+            cout<<current_sum;
+            cout<<" }";
+            if(end != n-1)
+            cout<<" , ";
+        }
+        cout<<endl;
+    }
+}
 int main(){
     int n ;
     cout<<"Enter the Size of Array:";
@@ -28,6 +45,8 @@ int main(){
     }
     cout<<"\nALL SUB ARRAY:\n";
     print_sub_array(v);
+    cout<<"\nALL SUB ARRAY SUM:\n";
+    print_sub_array_sum(v);
     
 
     return 0;

d0d83c70d311518b1a4198dab3cd64799e747dcc
Satyam3128 <satyam2005suman@gmail.com>
2025-05-18 15:37:34 +0530

    Add two matrix

---
+++ 4_2D_ARRAY/2_ADD_two_matrix.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr1[3][3];
+    int arr2[3][3];
+    int ans[3][3];
+
+    for(int i = 0;i<3 ;i++){
+        for(int j = 0;j<3 ;j++){
+            cout<<"Enter the "<<i<<" "<<j<<" element of Array:";
+            cin>>arr1[i][j];
+        }
+    }
+
+    for(int i = 0;i<3 ;i++){
+        for(int j = 0;j<3 ;j++){
+            cout<<"Enter the "<<i<<" "<<j<<" element of Array:";
+            cin>>arr2[i][j];
+        }
+    }
+cout<<"\nArray[]:-\n";
+    for(int i = 0;i<3 ;i++){
+        for(int j = 0;j<3 ;j++){
+            cout<<arr1[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+cout<<"\nArray[]:-\n";
+    for(int i = 0;i<3 ;i++){
+        for(int j = 0;j<3 ;j++){
+            cout<<arr2[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+cout<<"\nAns[]:-\n";
+    for(int i = 0;i<3 ;i++){
+        for(int j = 0;j<3 ;j++){
+        ans[i][j] = arr1[i][j] + arr2[i][j];
+        cout<<ans[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+
+    
+
+    return 0;
+
+}

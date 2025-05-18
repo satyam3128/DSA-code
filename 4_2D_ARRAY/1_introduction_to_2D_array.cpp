7ef4d542a819ffc84523d86b80695ff865180a9c
Satyam3128 <satyam2005suman@gmail.com>
2025-05-18 15:24:55 +0530

    intial commit

--- 4_2D_ARRAY/1_introduction_to_2D_array.cpp
+++ 4_2D_ARRAY/1_introduction_to_2D_array.cpp
@@ -1,29 +1,35 @@
 #include<iostream>
 using namespace std;
+
+
 int main(){
-    int arr[5][5];
-    int col;
-    int row;
-    cout<<"Enter the ROW:";
-    cin>>row;
-    cout<<"Enter the COLUMN:";
-    cin>>col;
-    for(int i = 0;i<row;i++){
-        for(int j = 0;j<col;j++){
+    int arr[4][3];
+    
+    for(int i = 0;i<3;i++){
+        for(int j = 0;j<4;j++){
             cout<<"Enter "<<i<<" "<<j<<" Element of Array:";
             cin>>arr[i][j];
         }
     }
-    for(int i = 0;i<row;i++){
-        for(int j = 0;j<col;j++){
+    for(int i = 0;i<3;i++){
+        for(int j = 0;j<4;j++){
             cout<<arr[i][j]<<" ";
         }
         cout<<endl;
     }
+    int x =7;
+    for(int i = 0 ;i< 3 ;i++){
+        for(int j = 0;j< 4 ;j++){
+            if(arr[i][j] == x){
+                cout<<"Yes Found :";
+                return 0;
+            }
+        }
 
-    
+    }
+    cout<<"Not Found ";
 
-     
+
 
     return 0;
 }

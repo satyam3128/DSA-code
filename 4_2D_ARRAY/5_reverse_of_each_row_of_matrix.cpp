713e0e6edf481529c2b72b06c39c00115e1b70c9
Satyam3128 <satyam2005suman@gmail.com>
2025-05-18 16:21:20 +0530

    reverse of each row of Matrix

---
+++ 4_2D_ARRAY/5_reverse_of_each_row_of_matrix.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[10][10];
+    int row;
+    int col;
+    cout<<"Row of Matrix:";
+    cin>>row;
+    cout<<"Column of Matrix:";
+    cin>>col;
+    for(int i = 0;i<row;i++){
+        for(int j = 0;j<col;j++){
+            cout<<"Enter "<<i<<" "<<j<<" Element of Array:";
+            cin>>arr[i][j];
+        }
+    }
+    for(int i = 0;i<row;i++){
+        for(int j = 0;j<col;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    // Revers of Each row
+    
+    for(int i = 0;i<row ;i++){
+    int j = 0;
+    int k = col-1;
+        while(j<=k){
+            int temp = arr[i][j];
+            arr[i][j] = arr[i][k];
+            arr[i][k] = temp;
+            j++;
+            k--;
+        }
+    }
+    cout<<"\nReversed Each Row of Matrix :\n";
+    for(int i = 0;i<row;i++){
+        for(int j = 0;j<col;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+
+
+    return 0;
+}

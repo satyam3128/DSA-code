33c3ce210323a2a3cc11b7d6ac69c0989f4dea2a
Satyam3128 <satyam2005suman@gmail.com>
2025-05-19 19:24:39 +0530

    commit changes

--- 4_2D_ARRAY/6_2_D_vector.cpp
+++ 4_2D_ARRAY/6_2_D_vector.cpp
@@ -3,17 +3,35 @@
 #include<algorithm>
 using namespace std;
 int main(){
-    
+    int n , m;
+    cout<<"Enter the row and column:";
+    cin>>n>>m;
     vector<vector<int> >matrix(3,vector<int>(4));
-    vector<vector<int> >matrix2(3,vector<int>(4,8));
-    for(int i = 0;i<3 ;i++){
-     for(int j = 0 ;j<4 ;j++){
-        cout<<matrix2[i][j]<<" ";
-     }
+    vector<vector<int> >matrix2(n,vector<int>(m));
+    // for(int i = 0;i<n ;i++){
+    //  for(int j = 0 ;j<m ;j++){
+    //     cout<<matrix2[i][j]<<" ";
+    //  }cout<<endl;
+    // }
+
+    // cout<<"\nRows:"<<matrix2.size();
+    // cout<<"\ncol:"<<matrix2[0].size();
+
+
+    for(int i = 0;i<n ;i++){
+        for(int j = 0 ;j<m ;j++){
+            cout<<"Enter the number:";
+            cin>>matrix2[i][j];
+        }
     }
 
-    cout<<"\nRows:"<<matrix2.size();
-    cout<<"\ncol:"<<matrix2[0].size();
+    for(int i = 0;i< n ;i++){
+        for(int j = 0;j<m ;j++){
+            cout<<matrix2[i][j]<<" ";
+
+        }
+        cout<<endl;
+    }
      
 
     return 0;

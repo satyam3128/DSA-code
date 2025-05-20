9a1eb304e4ff6101e05c7fd69fa1998567fbf848
Satyam3128 <satyam2005suman@gmail.com>
2025-05-21 02:10:21 +0530

    transpose of matrix

---
+++ 4_2D_ARRAY/10_transpose_of_matrix.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+#include <algorithm>
+using namespace std;
+int main(){
+    int row ;
+    int col ;
+    cout<<"Enter the number of Row:";
+    cin>>row;
+    cout<<"Enter the number of Column:";
+    cin>>col;
+    vector<vector<int> >matrix(row,vector<int>(col));
+
+    cout<<"\nEnter the Elements of Matrix:\n";
+    for(int i = 0;i< row;i++){
+        for(int j = 0; j<col;j++){
+        cout<<"Enter "<<i<<","<<j<<" element :";
+        cin>>matrix[i][j];
+        }
+    }
+    cout<<"\nMATRIX[][]:-\n";
+
+    for(int i = 0;i< row ;i++){
+        for(int j = 0;j< col ;j++){
+            
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    int arr[row][col];
+    for(int i = 0;i<row ;i++){
+        for(int j = 0 ;j<col ;j++){
+            arr[j][i] = matrix[i][j];
+            
+        }
+       
+    }
+
+    cout<<"\nTranspsose of matrix[]\n";
+    for(int i = 0;i<row ;i++){
+        for(int j = 0 ;j<col ;j++){
+            cout<<arr[i][j]<<" ";
+            
+
+        }
+        cout<<endl;
+       
+    }
+    
+
+
+    return 0;
+}

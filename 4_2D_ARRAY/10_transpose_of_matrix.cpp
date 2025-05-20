c4f7a20cd1f6fa00bf4193dd699fd70f97db13ce
Satyam3128 <satyam2005suman@gmail.com>
2025-05-21 02:16:20 +0530

    trnspose of matrix

--- 4_2D_ARRAY/10_transpose_of_matrix.cpp
+++ 4_2D_ARRAY/10_transpose_of_matrix.cpp
@@ -10,7 +10,6 @@ int main(){
     cout<<"Enter the number of Column:";
     cin>>col;
     vector<vector<int> >matrix(row,vector<int>(col));
-
     cout<<"\nEnter the Elements of Matrix:\n";
     for(int i = 0;i< row;i++){
         for(int j = 0; j<col;j++){
@@ -19,7 +18,6 @@ int main(){
         }
     }
     cout<<"\nMATRIX[][]:-\n";
-
     for(int i = 0;i< row ;i++){
         for(int j = 0;j< col ;j++){
             
@@ -28,27 +26,24 @@ int main(){
         cout<<endl;
     }
 
-    int arr[row][col];
-    for(int i = 0;i<row ;i++){
-        for(int j = 0 ;j<col ;j++){
-            arr[j][i] = matrix[i][j];
-            
+    // int arr[row][col];
+    // for(int i = 0;i<row ;i++){
+    //     for(int j = 0 ;j<col ;j++){
+    //         arr[j][i] = matrix[i][j];
+    //     }
+    // }
+    for(int i = 0 ;i<row-1; i++){
+        for(int j = i+1;j<col ;j++ ){
+            swap(matrix[i][j] , matrix[j][i]);
         }
-       
     }
-
     cout<<"\nTranspsose of matrix[]\n";
     for(int i = 0;i<row ;i++){
         for(int j = 0 ;j<col ;j++){
-            cout<<arr[i][j]<<" ";
-            
-
+            cout<<matrix[i][j]<<" ";
         }
         cout<<endl;
-       
     }
-    
-
-
+     
     return 0;
 }

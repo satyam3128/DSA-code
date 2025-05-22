c0a5d59fad3b897cfba1471be1aedc0cf1d3ee1a
Satyam3128 <satyam2005suman@gmail.com>
2025-05-22 11:45:36 +0530

    size of matrix fix

--- 4_2D_ARRAY/12_rotate_matrix_by_90_clockwise_more_optimeized_way.cpp
+++ 4_2D_ARRAY/12_rotate_matrix_by_90_clockwise_more_optimeized_way.cpp
@@ -2,38 +2,36 @@
 #include<vector>
 using namespace std;
 int main(){
-    int row, col;
-    cout<<"Enter the Row :";
-    cin>>row;
-    cout<<"Enter the Column:";
-    cin>>col;
-    vector<vector<int>>matrix(row,vector<int>(col));
+    int n ;
+    cout<<"Enter the size:";
+    cin>>n;
+    
+    vector<vector<int>>matrix(n,vector<int>(n));
     cout<<"Enter the Element of Matrix:\n";
-    for(int i = 0;i< row ;i++){
-        for(int j = 0;j< col ;j++){
+    for(int i = 0;i< n ;i++){
+        for(int j = 0;j< n ;j++){
             cout<<"Enter the "<<i<<","<<j<<" element of Array:";
             cin>>matrix[i][j];
         }
     }
     cout<<endl;
-    for(int i = 0;i< row ;i++){
-        for(int j = 0;j< col ;j++){
+    for(int i = 0;i< n ;i++){
+        for(int j = 0;j< n ;j++){
             cout<<matrix[i][j]<<" ";
         }
         cout<<endl;
     }
 
     // transpose of matrix
-    for(int i  = 0;i<row-1;i++){
-        for(int j = i+1 ;j<col ;j++){
+    for(int i  = 0;i<n-1;i++){
+        for(int j = i+1 ;j<n ;j++){
             swap(matrix[i][j] , matrix[j][i]);
         }
-    }
 
     // reversing the rowise 
-    for(int i = 0;i< row ;i++){
+    for(int i = 0;i< n ;i++){
         int start  =0 ;
-        int end = col-1 ;
+        int end = n-1 ;
         while(start<end){
             swap(matrix[i][start] ,  matrix[i][end]);
             start++;
@@ -42,13 +40,13 @@ int main(){
     }
     
     cout<<"\nRotated Matrix:\n";
-    for(int i = 0;i< row ;i++){
-        for(int j = 0;j< col ;j++){
+    for(int i = 0;i< n ;i++){
+        for(int j = 0;j< n ;j++){
             cout<<matrix[i][j]<<" ";
         }
         cout<<endl;
     }
-
-
+    
+    }
     return 0;
 }

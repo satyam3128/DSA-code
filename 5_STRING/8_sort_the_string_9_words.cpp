37c1f33fb824d815c5c66e8995f747b1bec87dea
Satyam3128 <satyam2005suman@gmail.com>
2025-05-31 22:54:48 +0530

    sort string of 9 words

---
+++ 5_STRING/8_sort_the_string_9_words.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+string sentence_form(string &sen){
+    vector<string>ans(10);
+    int count = 0;
+    int index = 0;
+    string temp;
+
+    while(index<sen.size()){
+        if(sen[index] == ' '){
+            int pos = temp[temp.size()-1] - '0';
+            temp.pop_back();
+            ans [pos] = temp;
+            temp.clear();
+            count++;
+
+        }else{
+            temp += sen[index];
+        }
+        index++;
+    }
+    int pos = temp[temp.size()-1]-'0';
+    temp.pop_back();
+    ans[pos] = temp;
+    temp.clear();
+    count++;
+
+    for(int i =1 ; i<=count;i++){
+        temp = temp + ans[i];
+        temp = temp + ' ';
+    }
+    temp.pop_back();
+
+    return temp;
+}
+
+
+
+int main(){
+
+    string sen;
+    cout<<"Enter the Unressemble words:";
+    getline(cin,sen);
+
+    cout<<"Ressembled words and a complete sentence:";
+    cout<<sentence_form(sen);
+
+
+
+    return 0;
+}

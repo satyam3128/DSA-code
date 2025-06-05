10626a61a9b632b72124cb418a6804a13594d769
Satyam3128 <satyam2005suman@gmail.com>
2025-06-05 13:45:46 +0530

    factorial of large number

--- 5_STRING/14_factorial_of_large_number.cpp
+++ 5_STRING/14_factorial_of_large_number.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
+vector<int>factorial(int n ){
+    vector<int>ans(1,1);
+    while(n>1){
+        int carry = 0 ;
+        int size = ans.size();
+        int res;
+        for(int i = 0;i<size ;i++){
+            res = ans[i] * n + carry ;
+            carry = res /10;
+            ans[i] = res%10;
+
+        }
+        while(carry){
+            ans.push_back(carry % 10 );
+            carry /=10;
+        }
+        n--;
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+}
+
 int main(){
     
+    int n ;
+    cout<<"Enter the number:";
+    cin>>n;
+
+    vector<int>fac = factorial(n);
+    for(int i = 0;i<fac.size() ;i++){
+        cout<<fac[i];
+        
+    }
 
     return 0;
 }

2ebfb7d379950170f69bb81ed199af4aaa75bf45
Satyam3128 <satyam2005suman@gmail.com>
2025-06-07 00:04:17 +0530

    intial commit

--- 2_Pattern_program/15_triangle_type_verticle.cpp
+++ 2_Pattern_program/15_triangle_type_verticle.cpp
@@ -5,10 +5,11 @@ int main(){
     cout<<"Enter the Number :";
     cin>>num;
     for(int i = 1 ;i<=num;i++){
-        
+        // space printing
         for(int j = 1 ;j<=num-i;j++){
             cout<<" ";
         }
+        // star printing 
         for(int k = 1 ;k<=i;k++){
             cout<<"*";
         }
@@ -16,10 +17,11 @@ int main(){
     }
 
     for(int i = 1 ;i<=num;i++){
-        // space
+        // space printing
         for(int j = 1 ;j<=i;j++){
             cout<<" ";
         }
+        // star printing
         for(int k = 1 ;k<=num-i;k++){
             cout<<"*";
 

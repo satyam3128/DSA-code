48d2f796069d1ccbeed2bf001f62ae98accf7d50
Satyam3128 <satyam2005suman@gmail.com>
2025-06-10 19:40:27 +0530

    frequency of any char

---
+++ 7_HASHING_CONCEPT/3_string_frequency_of_any_char.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    string s;
+    cout<<"Enter the string:";
+    getline(cin,s);
+
+    // frequency store
+    vector<int>hashh(256,0);
+    for(int i = 0;i<s.size();i++){
+        hashh[s[i]]++;
+    }
+
+    char ch;
+    cout<<"Enter char:";
+    cin>>ch;
+
+    cout<<"Frequency:";
+    for(int i = 0;i<hashh.size();i++){
+        if(i == ch){
+            cout<<hashh[i];
+        }
+    }
+    return 0;
+}

6e9d1c9e4460fa1cd15c29aa8a0928b5fdcdbe35
Satyam3128 <satyam2005suman@gmail.com>
2025-06-10 19:37:20 +0530

    count frequency of upper letter

--- 7_HASHING_CONCEPT/2_string_freq_upper.cpp
+++ 7_HASHING_CONCEPT/2_string_freq_upper.cpp
@@ -3,22 +3,22 @@
 using namespace std;
 int main(){
     string s;
-    cout<<"Enter the  lower letter string:";
+    cout<<"Enter the upper letter string:";
     getline(cin,s);
 
     // frequency store
     vector<int>hashh(26,0);
     for(int i = 0;i<s.size();i++){
-        hashh[s[i] - 'a']++;
+        hashh[s[i] - 'A']++;
     }
 
     char ch;
-    cout<<"Enter the small letter char:";
+    cout<<"Enter capital letter char:";
     cin>>ch;
 
     cout<<"Frequency:";
     for(int i = 0;i<hashh.size();i++){
-        if(i + 'a' == ch){
+        if(i + 'A' == ch){
             cout<<hashh[i];
         }
     }

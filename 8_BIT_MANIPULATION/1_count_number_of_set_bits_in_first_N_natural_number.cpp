5b57bb5204474491fee18e63ff3b790c655def0d
Satyam3128 <satyam2005suman@gmail.com>
2025-06-13 08:40:58 +0530

    set bit count

---
+++ 8_BIT_MANIPULATION/1_count_number_of_set_bits_in_first_N_natural_number.cpp
@@ -0,0 +1,32 @@
+// Brute Force Approch
+#include <iostream>
+using namespace std;
+int count_set_bit(int num)
+{
+    int count = 0;
+    while (num)
+    {
+        int rem = num % 2;
+        num = num / 2;
+        if (rem == 1)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int main()
+{
+    int num;
+    cout << "Enter the Number :";
+    cin >> num;
+
+    int count_one = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        count_one = count_one + count_set_bit(i);
+    }
+    cout<<"Set Bit :"<<count_one;
+
+    return 0;
+}

2b3352c65a089eae82d79f7c29f6f1bd4ccd5c20
Satyam3128 <satyam2005suman@gmail.com>
2025-06-16 13:24:21 +0530

    stair path for 3 step also jump possible

--- 9_RECURSION/8_stair_path_steps_no_of_ways_calulate.cpp
+++ 9_RECURSION/8_stair_path_steps_no_of_ways_calulate.cpp
@@ -1,13 +1,16 @@
-// condition: at a time, jump is either one step or two steps only
+// conditions: at a time, jump are either one step or two steps 
+// Now also incude 3 step possible to jump at a time 
 
 #include<iostream>
 using namespace std;
 int combination(int stair){
-    if(stair == 2) return 2;
+
     if(stair == 1) return 1;
+    if(stair == 2) return 2;
+    if(stair == 3) return 4;
     
 
-    return combination(stair-1) + combination(stair-2);
+    return combination(stair-1) + combination(stair-2) + combination(stair-3);
 }
 int main(){
     int num;

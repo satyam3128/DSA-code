a21c56e38e4b68dce692d4df6baf4cef9277b982
Satyam3128 <satyam2005suman@gmail.com>
2025-06-16 14:11:30 +0530

    maze path move right and   down

---
+++ 7_RECURSION/9_maze_path_two_direction_allow_right_or_down_only.cpp
@@ -0,0 +1,39 @@
+// Condition:
+// 1: only Right and down Direction allowed to move 
+// 2:Find all The possible ways 
+
+#include<iostream>
+using namespace std;
+int maze_path(int start_row , int start_col ,  int end_row , int end_col){
+    
+    if(start_row > end_row  || start_col >end_col) return 0;
+
+    if(start_row == end_row && start_col == end_col) return 1;
+    // if first move to right side
+    int rightways = maze_path(start_row , start_col+1 ,end_row ,end_col);
+    // if first move to down side
+
+    int downways = maze_path(start_row +1 , start_col ,end_row,end_col);
+
+    return rightways + downways;
+}
+int main(){
+
+    int start_row;
+    int start_col;
+    int final_row;
+    int final_col;
+
+    cout<<"Enter intial Position Row:";
+    cin>>start_row;
+    cout<<"Enter intial Position col:";
+    cin>>start_col;
+    cout<<"Enter final Destination Row:";
+    cin>>final_row;
+    cout<<"Enter final Destination col:";
+    cin>>final_col;
+
+    cout<<"TOTAL WAYS :"<<maze_path(start_row , start_col,final_row,final_col);
+
+    return 0;
+}

76244cb925f914c4997d02f1513f3639a1c4d0ee
Satyam3128 <satyam2005suman@gmail.com>
2025-06-17 12:16:30 +0530

    commit changes

--- 7_RECURSION/8_stair_path_steps_no_of_ways_calulate.cpp
+++ 7_RECURSION/8_stair_path_steps_no_of_ways_calulate.cpp
@@ -1,15 +1,13 @@
-// conditions: at a time, jump are either one step or two steps 
-// Now also incude 3 step possible to jump at a time 
+// conditions:-
+// 1. ONE , TWO ,THREE steps jump at a time :
+// 2.Count all the Possible way to reach At Final Step.
 
 #include<iostream>
 using namespace std;
 int combination(int stair){
-
     if(stair == 1) return 1;
     if(stair == 2) return 2;
     if(stair == 3) return 4;
-    
-
     return combination(stair-1) + combination(stair-2) + combination(stair-3);
 }
 int main(){
@@ -17,6 +15,5 @@ int main(){
     cout<<"Enter the number of Steps:";
     cin>>num;
     cout<<"No OF Possible Combination:"<<combination(num);
-
     return 0;
 }

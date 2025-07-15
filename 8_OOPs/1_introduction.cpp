b7223f4269a439838da1183ec7a5313183cfc1bc
Satyam3128 <satyam2005suman@gmail.com>
2025-07-15 22:57:26 +0530

    intial commit

---
+++ 8_OOPs/1_introduction.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+class student{
+    public:
+    string name;
+    int age;
+    int roll_number;
+    string grade;
+
+    
+
+};
+int main(){
+
+    student s1;
+    s1.name = "Satyam";
+    s1.age = 25;
+    s1.roll_number = 21;
+    s1.grade = "A+";
+
+    
+    return 0;
+}
